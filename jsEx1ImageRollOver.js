8c95d8d4f842267f826010c6bf6a5209223918da
Kavana N <149854591+KavanaN12@users.noreply.github.com>
2025-10-16 12:00:04 +0530

    Add rollover image example in jsEx1ImageRollOver.js

---
+++ jsEx1ImageRollOver.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rollover Image Example</title>
+</head>
+<body>
+  <img id="myImage"
+       src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png"
+       alt="Rollover Example"
+       width="200"
+       onmouseover="this.src='https://img.jagranjosh.com/images/2022/November/28112022/camel-flu.jpg';"
+       onmouseout="this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png';"
+  >
+</body>
+</html>
